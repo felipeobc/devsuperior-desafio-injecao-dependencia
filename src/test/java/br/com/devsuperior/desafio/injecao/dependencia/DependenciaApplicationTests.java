ece6355338efedbc4fb2fa85c7f5e9bb6ac50ba9
Felipe Camargo <felipe.c@outlook.com>
2024-09-12 21:00:28 -0300

    update

---
+++ src/test/java/br/com/devsuperior/desafio/injecao/dependencia/DependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.desafio.injecao.dependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
